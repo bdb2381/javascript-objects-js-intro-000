3fe26c7da45b61e320b811cca819b7f8fec1d6af
Bradley Beggs <brad.beggs@gmail.com>
2020-06-11 02:57:50 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,7 @@ var playlist = {'King': 'song'}
 
 function updatePlaylist(playlist, Queen, free ){
 
-
-  return Object.assign({} playlist, {[key]: value})
+  playlist[key] = value
+  return
+  // Object.assign({} playlist, {[key]: value})
 }
